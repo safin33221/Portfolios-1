200bd073168f992fd6b7cb56d9eb9599a63df200
safin33221 <saifn33221@gmail.com>
2024-11-03 14:38:17 +0600

    initalize setup complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold'>hey this is safin</h1>
+      <button className="btn btn-primary">button</button>
+    </div>
+  );
+};
+
+export default App;
